689daa54eb58134f1e59a5e1626c01e601a45e00
Ronnie Goodrich <kahootbird@gmail.com>
2022-01-06 21:02:42 -0500

    Cleaned code & fixed function syntax to ES6

--- comps/Key.js
+++ comps/Key.js
@@ -3,24 +3,19 @@ import React, {useEffect, useState } from "react";
 import styles from '../styles/Home.module.css'
 
 const Key = (props) => {
-
 const [keyvalue, setKeyValue] = useState(0)
-
-function click_btn()
+const click_btn = () =>
 {
   setKeyValue(1)
   //Call passed function through props to pass it up the chain to index.js calling tone.js for music
   props.call_playsound(props.keynote)
   setTimeout(fix_keypress,200)
-
 }
-function fix_keypress()
+const fix_keypress = () =>
 {
   setKeyValue(0)
 }
-
 var cmdDown = false;
-
 //noteIsFlat to determine if giving flat key div or regular div
 var noteIsFlat = 0
 if (props.flat == true)
@@ -29,13 +24,11 @@ var info
 if (noteIsFlat == 0)
 {
   if (keyvalue == 0)
-
   //      {props.keynote}
   var info = (
     <piano>
     <div className="container" onClick={click_btn}>
       <div className={styles.key}>
-
       </div>
     </div>
     </piano>
@@ -45,7 +38,6 @@ if (noteIsFlat == 0)
       <piano>
       <div className="container" onClick={click_btn}>
         <div className={styles.keypressed}>
-
         </div>
       </div>
       </piano>
@@ -58,16 +50,11 @@ else
     <piano>
     <div className="container">
       <div className={styles.keyflat} onClick={click_btn}>
-
       </div>
     </div>
     </piano>
   )
 }
-
 return(info)
-
 }
-
-
 export default Key
